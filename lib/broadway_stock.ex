1fb703fe264049c0f77330f25e2cfe07eba01035
nmenag <nmena.garzon@gmail.com>
2020-11-13 22:40:23 -0500

    init commit

---
+++ lib/broadway_stock.ex
@@ -0,0 +1,18 @@
+defmodule BroadwayStock do
+  @moduledoc """
+  Documentation for `BroadwayStock`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BroadwayStock.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
