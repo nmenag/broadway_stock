2b992a8aee075ce3dd5fe92c21372a70fd7f32e3
nmenag <nmena.garzon@gmail.com>
2020-11-13 23:07:39 -0500

    Add broadway worker logic

--- lib/broadway_stock.ex
+++ lib/broadway_stock.ex
@@ -1,18 +1,28 @@
 defmodule BroadwayStock do
-  @moduledoc """
-  Documentation for `BroadwayStock`.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  @csv_file "./data/companylist.csv"
+  @queue_name "stock_queue"
 
-  ## Examples
+  def dispatch do
+    {:ok, connection} = AMQP.Connection.open()
+    {:ok, channel} = AMQP.Channel.open(connection)
+    AMQP.Queue.declare(channel, @queue_name, durable: true)
 
-      iex> BroadwayStock.hello()
-      :world
+    @csv_file
+    |> File.stream!()
+    |> Stream.drop(1)
+    |> Stream.map(&String.trim(&1, "\n"))
+    |> Stream.map(&String.split(&1, ","))
+    |> Enum.map(fn columns -> List.first(columns) end)
+    |> Enum.each(fn symbol ->
+      write_queue(channel, symbol)
+    end)
 
-  """
-  def hello do
-    :world
+    AMQP.Connection.close(connection)
+  end
+
+  defp write_queue(channel, symbol) do
+    AMQP.Basic.publish(channel, "", @queue_name, symbol)
   end
 end
